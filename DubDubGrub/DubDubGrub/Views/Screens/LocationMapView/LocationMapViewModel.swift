9281b14b8f1578edb672d3793b6be55ff0f45583
haroldstest <dongik369@naver.com>
2024-12-21 11:55:52 +0900

    Dependency Injection

--- DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
@@ -9,20 +9,20 @@ import MapKit
 
 final class LocationMapViewModel: ObservableObject {
     
+    @Published var alertItem: AlertItem?
     @Published var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
         span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     
-    @Published var alertItem: AlertItem?
-    @Published var locations: [DDGLocation] = []
-    
-    func getLocations() {
+    func getLocations(for locationManager: LocationManager) {
         CloudKitManager.getLocation { [self] result in
-            switch result {
+            DispatchQueue.main.async {
+                switch result {
                 case .success(let locations):
-                self.locations = locations
-            case .failure(_):
-                alertItem = AlertContext.unableToGetLocations
+                    locationManager.locations = locations
+                case .failure(_):
+                    alertItem = AlertContext.unableToGetLocations
+                }
             }
         }
     }
